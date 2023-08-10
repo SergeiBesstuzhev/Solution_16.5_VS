c700c2c0b3025b340cfe4f8fd171c408784e3cc3
Sergei Bess <sergeibesstuzhev1989@gmail.com>
2023-08-10 20:47:23 +0300

    Добавьте файлы проекта.

---
+++ Solution_16.5_VS.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <time.h>
+
+int main()
+{
+	int array[5][5];
+	int N = 0;
+	struct tm buf;
+	time_t t = time(NULL);
+	localtime_s(&buf, &t);
+
+	for (int i = 0; i < 5; i++) {
+		for (int j = 0; j < 5; j++) {
+			array[i][j] = i + j;
+		}
+	}
+
+	for (int i = 0; i < 5; i++) {
+		for (int j = 0; j < 5; j++) {
+			std::cout << array[i][j];
+		}
+		std::cout << std::endl;
+	}
+
+	std::cout << "Enter N: ";
+	std::cin >> N;
+
+	for (int i = 0; i < 5; i++) {
+		std::cout << array[buf.tm_mday % N][i];
+	}
+}
